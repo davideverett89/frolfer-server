ba0e6525b53ee20149e0d53ec73792aa14a740c4
David Everett <davideverett1989@gmail.com>
2021-04-02 00:40:10 -0500

    Made adjustments based on front end needs.

--- frolferapi/views/auth.py
+++ frolferapi/views/auth.py
@@ -3,8 +3,8 @@ import json
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
-from rest_framework.authtoken.models import Token
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.authtoken.models import Token
 from frolferapi.models import Player
 
 @csrf_exempt
