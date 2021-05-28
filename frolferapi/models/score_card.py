3a9b4fb1a70d00a81b1e0acf9a1a6ac5698a6923
David Everett <davideverett1989@gmail.com>
2021-05-28 11:51:16 -0500

    Adjusted models.

--- frolferapi/models/score_card.py
+++ frolferapi/models/score_card.py
@@ -10,6 +10,5 @@ class ScoreCard(models.Model):
     """
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    player = models.ForeignKey(Player, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.DO_NOTHING)
     condition = models.CharField(max_length=50, default="")
--- frolferapi/serializers/score_card.py
+++ frolferapi/serializers/score_card.py
@@ -14,6 +14,6 @@ class ScoreCardSerializer(serializers.ModelSerializer):
             'id',
             'start_time',
             'end_time',
-            'player',
-            'course'
+            'course',
+            'condition'
         )
